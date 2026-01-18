9b7b88b6cd02ff5bc09ad223968f2b963a4d2269
c0l0rbl1ndn355 <c0l0r.bl1nd.n355@gmail.com>
2026-01-17 20:03:32 -0500

    Added Win, Loss, and Draw conditions and checks

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,11 +1,24 @@
 #Tic Tac Toe game vs a CPU
 import random
-#Allows X and O to be assigned to the grid
+#Allows player and cpu turns to be assigned to the grid
 grid_num = [0,
     1, 2, 3,
     4, 5, 6,
     7, 8, 9]
     
+#All possible win combonations
+WIN_COMBOS = [
+    (1, 2, 3),  # rows
+    (4, 5, 6),
+    (7, 8, 9),
+    (1, 4, 7),  # columns
+    (2, 5, 8),
+    (3, 6, 9),
+    (1, 5, 9),  # diagonals
+    (3, 5, 7)
+]
+
+#Grid of the game being printed on screen
 def make_grid(grid_num):
     return f"""
 {grid_num[1]} | {grid_num[2]} | {grid_num[3]}
@@ -15,16 +28,37 @@ def make_grid(grid_num):
 {grid_num[7]} | {grid_num[8]} | {grid_num[9]}
 """
 
+#CPU turn
 def cpu_turn():
     while True:
         cpu_num = random.randint(1, 9)
+        #Checks if move is valid
         if grid_num[cpu_num] != "X" and grid_num[cpu_num] != "O":
             grid_num[cpu_num] = cpu
             break
 
+#Checks to see if the game is won  
+def check_winner(grid, symbol):
+    for a, b, c in WIN_COMBOS:
+        if grid[a] == grid[b] == grid[c] == symbol:
+            return True
+    return False
+
+#Checks to see if the game is a draw
+def is_draw(grid):
+    for i in range(1, 10):
+        if grid[i] != "X" and grid[i] != "O":
+            return False
+    return True
 player = input("Choose either 'X' or 'O' ")
+#Updates the Player letter to be Capital
+if player == "x":
+    player = "X"
+elif player == "o":
+    player = "O"
 cpu = ""
 
+#sets the cpu letter to the opposite of the player's letter
 if player == "X" or player == "x":
     cpu = "O"
 elif player == "O" or player == "o":
@@ -50,6 +84,15 @@ for num in range(1, 10):
     cpu_num = int(random.randint(1, 9))
     cpu_turn()
     
+    if check_winner(grid_num, player):
+        print("You win!")
+        break
+    elif check_winner(grid_num, cpu):
+        print("CPU wins!")
+        break
+    elif is_draw(grid_num):
+        print("Draw!")
+        break
             
             
     print(make_grid(grid_num))
